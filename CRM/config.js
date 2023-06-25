1acd15dd33e96f12c6158ada3508f1e895fca3da
Vraj Patel <vraj.patel.12390@gmail.com>
2023-06-25 19:37:36 -0400

    frontend added

---
+++ CRM/config.js
@@ -0,0 +1,8 @@
+// var url="https://crm-web-api.herokuapp.com";
+// var url="https://crmapiexcel.herokuapp.com" // Paste API URL here.
+var url="http://localhost:3000"
+module.exports={url};
+
+
+
+
